ad3f589fe6fe8a7b181d925bbd6fb7c858beb21b
seojungmin123 <162937279+seojungmin123@users.noreply.github.com>
2024-11-30 22:53:34 +0900

    Add files via upload

---
+++ if_test.py
@@ -0,0 +1,29 @@
+'''
+x = 10
+
+if x>9:
+    print ("실행되었습니다")
+
+    
+x = int (input ("정수 하나를 입력하세요:"))
+    
+if x % 2 == 0:
+    print("짝수입니다")
+else:
+    print("홀수입니다")             
+
+    
+password = input("암호를 문자로 입력하세요")
+
+if password == "qwer1234":
+    print("암호이다")
+else:
+    print("암호가아니다")
+'''
+
+age = 17
+
+if age>15:
+    print("참")
+else:
+    print("거짓")
