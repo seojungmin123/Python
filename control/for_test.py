dded0057595977679c47fce5ae1c3bd4d2b84373
seojungmin123 <162937279+seojungmin123@users.noreply.github.com>
2024-12-11 23:27:10 +0900

    Add files via upload

---
+++ control/for_test.py
@@ -0,0 +1,48 @@
+'''
+count = 0
+for ch in "서 정 민":
+    print(ch, end='')
+    count = count + 1
+print()
+print(count)
+
+
+food_list = ["치킨", "피자", "햄버거", "찐계란", "떡볶이", "자장면"]
+for food in food_list:
+    print(food)
+    
+
+i = [1,2,3,4,5]
+for x in i:
+    print(x, end='')
+
+
+for n in [1,2,3]:
+    print("\nAI 화이팅")
+
+
+for item in ["힙합","발라드"]:
+    print(item + " 즐겨듣는다")
+
+
+
+count = 0
+for n in range(300):
+    print(f"{count}만큼 사랑해")
+    count = count+1
+
+
+for n in range(10, 1, -2):
+    print(n)
+
+
+for n in range(3, 31, 3):
+    print(n)
+
+
+for n in range(10, 21, 1):
+    print(n)
+'''
+
+for n in range(97, 76, -1):
+    print(n)
