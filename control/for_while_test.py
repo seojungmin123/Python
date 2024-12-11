552049dd82c96f4875d10882a6f5d7b8344f89c1
seojungmin123 <162937279+seojungmin123@users.noreply.github.com>
2024-12-11 23:31:52 +0900

    for문(range) 실습 과제

---
+++ control/for_while_test.py
@@ -0,0 +1,19 @@
+'''
+sum_number = 0
+for i in range(101):
+    sum_number = sum_number + i
+print(f"1~100 까지의 합: {sum_number})
+
+
+for i in range(1, 10):
+    print(f"3*{i}={3*i}")
+'''
+
+for i in range(2,10):
+    print(f"\n{i}단")
+    
+    for j in range(1,10):
+        print(f"{i} * {j} = {i*j}")
+        
+    
+    
